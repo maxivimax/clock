c118805a5a34551811984672adc0893dd66c1be7
Вячеслав Андреев <53828303+maxivimax@users.noreply.github.com>
2021-01-11 09:22:29 +0300

    Update flagsANDscripts.js

--- flagsANDscripts.js
+++ flagsANDscripts.js
@@ -69,5 +69,5 @@
 
 	 document.getElementById('time').innerHTML = h + ':' + m + ":" + s; 
 		
-	 document.getElementById('dmy').innerHTML = datee + ' ' + mouth + ' ' + yearr;  
+	 document.getElementById('dmy').innerHTML = datee + ' ' + mou + ' ' + yearr;  
 	}, 1000);
